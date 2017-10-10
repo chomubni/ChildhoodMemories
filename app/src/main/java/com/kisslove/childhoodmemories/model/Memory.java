79a3ef25b910ba8cc82c4a5e08ca8f92f1a34359
ivan kyselov <kyselovivan@gmail.com>
2017-10-10 14:54:32 +0300

    describe Memory entry

--- app/src/main/java/com/kisslove/childhoodmemories/model/Memory.java
+++ app/src/main/java/com/kisslove/childhoodmemories/model/Memory.java
@@ -1,8 +1,64 @@
 package com.kisslove.childhoodmemories.model;
 
+import java.util.Date;
+
 /**
  * Created by ivan on 10.10.17.
  */
 
 public class Memory {
+    private long memoryId;
+    private String title;
+    private String description;
+    private String photoPath;
+    private String videoPath;
+    private Date memoryDate;
+
+    public long getMemoryId() {
+        return memoryId;
+    }
+
+    public void setMemoryId(long memoryId) {
+        this.memoryId = memoryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public String getVideoPath() {
+        return videoPath;
+    }
+
+    public void setVideoPath(String videoPath) {
+        this.videoPath = videoPath;
+    }
+
+    public Date getMemoryDate() {
+        return memoryDate;
+    }
+
+    public void setMemoryDate(Date memoryDate) {
+        this.memoryDate = memoryDate;
+    }
 }
